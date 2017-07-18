0a27d791443173a6a91535755f5cbede82616cbd
Gaurav Sharma <gauravsharma.mvp@gmail.com>
2017-07-18 20:04:34 +0530

    correct one

--- DP/magicArray.cpp
+++ DP/magicArray.cpp
@@ -2,17 +2,18 @@
 using namespace std;
 int find(int a[],int start,int end){
 	
-	if(start<end){
-		int mid=(start+end)/2;
-		if(a[mid]==mid)return mid;
-		find(a,start,mid);
+	if(start>end)return -1;	
+	int mid=(start+end)/2;
+	if(a[mid]==mid)return mid;
+	else if(a[mid]>mid)
+		find(a,start,mid-1);
+	else
 		find(a,mid+1,end);
-	}
 }
 int main(){
 	int n;
 	cout<<"Length of array : ";cin>>n;
-	int a[n]={10,9,8,7,6,3,2,5,6,3,6,8,5,66,3,4,16};
+	int a[n]={-1,0,2,3,5,6,7};
 	cout<<find(a,0,n-1);
 	return 0;
 }
