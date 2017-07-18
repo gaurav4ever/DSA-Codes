e3984a00b62ef329e65877ba29c978899706b225
Gaurav Sharma <gauravsharma.mvp@gmail.com>
2017-07-18 20:41:40 +0530

    spelling mistake

--- DP/magicArray.cpp
+++ DP/magicArray.cpp
@@ -13,7 +13,7 @@ int find(int a[],int start,int end){
 int main(){
 	int n;
 	cout<<"Length of array : ";cin>>n;
-	int a[n]={-1,0,2,3w,5,6,7};
+	int a[n]={-1,0,2,3,5,6,7};
 	cout<<find(a,0,n-1);
 	return 0;
 }
