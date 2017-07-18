7c48585a1b969997a6da10756a0028c4926deffc
Gaurav Sharma <gauravsharma.mvp@gmail.com>
2017-07-18 18:51:13 +0530

    cached fibo

--- DP/fibo.cpp
+++ DP/fibo.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
 using namespace std;
-int fibo(int n){
-	if(n==0)return 0;
-	if(n==1)return 1;
-	return fibo(n-1) +fibo(n-2);
+int n;
+int f[10000];
+int count1=0,count2=0;
+int fibo1(int x){
+	count1++;
+	if(x==0)return 0;
+	if(x==1)return 1;
+	if(f[x]!=0)return f[x];
+	f[x]=fibo1(x-1) +fibo1(x-2);
+	return f[x];
+}
+int fibo2(int x){
+	count2++;
+	if(x==0)return 0;
+	if(x==1)return 1;
+	return fibo2(x-1)+fibo2(x-2);
 }
 int main(){
-	int n;
 	cout<<"Enter n to find nth term in finonacci series : ";
 	cin>>n;
-	cout<<n<<"th term is : "<<fibo(n);
+	cout<<n<<"th term is : "<<fibo1(n)<<endl;
+	fibo2(n);
+	cout<<"No of computations in 1 : "<<count1<<endl;
+	cout<<"No of computations in 2 : "<<count2;
 	return 0;
 }
