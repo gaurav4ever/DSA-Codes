03f8bb57d35658d19a724ed3221fc1ef73f07aaa
Gaurav Sharma <gauravsharma.mvp@gmail.com>
2017-07-18 21:46:05 +0530

    permutations of a string

---
+++ DP/permutationOfString.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+using namespace std;
+void swap(char *a,char *b){
+	char temp=*a;
+	*a=*b;
+	*b=temp;
+}
+void find(string s,int l,int r){
+	if(l==r)cout<<s<<endl;
+	else{
+		for(int i=l;i<=r;i++){
+			swap(&s[i],&s[l]);
+			find(s,l+1,r);
+			swap(&s[i],&s[l]);
+		}
+	}
+}
+int main(){
+	int n;
+	cout<<"Enter string size : ";
+	cin>>n;
+	string s;
+	cout<<"Enter string : ";
+	cin>>s;
+	find(s,0,n-1);
+}
