49e7f913369725e622aa50f098f082face2705bf
Gaurav Sharma <gauravsharma.mvp@gmail.com>
2017-07-19 17:02:05 +0530

    tower of hanoi added

---
+++ DP/towerOfHanoi.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void toh(int n,char from,char to,char buffer){
+	if(n==1){
+		cout<<"Disk "<<n<<" moved from "<<from<<" to "<<to<<endl;
+		return;
+	}
+	toh(n-1,from,buffer,to);
+	cout<<"Disk "<<n<<" moved from "<<from<<" to "<<to<<endl;
+	toh(n-1,buffer,to,from);
+}
+int main(){
+	int c=3;
+	toh(c,'a','b','c');
+	return 0;
+}
