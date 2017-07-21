b1d9169e77657fe8c8c97685ae39091e1ff02c64
Gaurav Sharma <gauravsharma.mvp@gmail.com>
2017-07-21 12:31:21 +0530

    small changes

--- questions/jessAndCookies2.cpp
+++ questions/jessAndCookies2.cpp
@@ -9,8 +9,8 @@ void heapify(struct heap *heapp,int i){
 	int large=i;
 	int l=2*i+1;
 	int r=2*i+2;
-	if(l<heapp->size && heapp->a[l]>heapp->a[large])large=l;
-	if(r<heapp->size && heapp->a[r]>heapp->a[large])large=r;
+	if(l<heapp->size && heapp->a[l]<heapp->a[large])large=l;
+	if(r<heapp->size && heapp->a[r]<heapp->a[large])large=r;
 	if(large!=i){
 		int temp=heapp->a[i];
 		heapp->a[i]=heapp->a[large];
@@ -30,31 +30,19 @@ struct heap* createHeap(int *a,int size){
 }
 struct heap* heap_sort(int *a,int size){
 	struct heap *heapp=createHeap(a,size);
-	int k=heapp->size;
-	while(heapp->size>1){
-		int temp=heapp->a[0];
-		heapp->a[0]=heapp->a[heapp->size-1];
-		heapp->a[heapp->size-1]=temp;
-		heapp->size-=1;
-		heapify(heapp,0);
-	}
-	heapp->size=k;
 	return heapp;
 }
 struct heap* del(struct heap *heapp){
 	heapp->a[0]=heapp->a[heapp->size-1];
+	int index;
+	if(heapp->a[1]<heapp->a[2])
+		index=1;
+	else
+		index=2;
+	heapp->a[index]=heapp->a[heapp->size-1];
 	heapify(heapp,0);
-
-	int k=heapp->size;
-	while(heapp->size>1){
-		int temp=heapp->a[0];
-		heapp->a[0]=heapp->a[heapp->size-1];
-		heapp->a[heapp->size-1]=temp;
-		heapp->size-=1;
-		heapify(heapp,0);
-	}
-	heapp->size=k;
-	heapp->size-=1;
+	heapify(heapp,index);
+	heapp->size-=2;
 	return heapp;
 }
 struct heap* insert(struct heap *heapp,int ele){
@@ -64,16 +52,6 @@ struct heap* insert(struct heap *heapp,int ele){
 	for(int i=(heapp->size-1)/2;i>=0;i--){
 		heapify(heapp,i);
 	}
-
-	int k=heapp->size;
-	while(heapp->size>1){
-		int temp=heapp->a[0];
-		heapp->a[0]=heapp->a[heapp->size-1];
-		heapp->a[heapp->size-1]=temp;
-		heapp->size-=1;
-		heapify(heapp,0);
-	}
-	heapp->size=k;
 	return heapp;
 }
 int main(){
@@ -85,16 +63,23 @@ int main(){
 	struct heap *heapp=heap_sort(a,n);
 
 	
-
+	int count=0;
 	while(1){
 		cout<<"\nSorted Array : ";
 		for(int i=0;i<heapp->size;i++)cout<<heapp->a[i]<<" ";
 
 		int min1=heapp->a[0];
 		if(min1<m){
+			count++;
+			
+			int min2=0;
+			if(heapp->a[1]<heapp->a[2])
+				min2=heapp->a[1];
+			else
+				min2=heapp->a[2];
+
 			heapp=del(heapp);
-			int min2=heapp->a[0];
-			del(heapp);
+
 			int new_val=min1+2*min2;
 			heapp=insert(heapp,new_val);
 		}else break;
