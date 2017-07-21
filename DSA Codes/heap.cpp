d8ad0312a1a29cb0a096a6f98b0f642f4c5f2d47
Gaurav Sharma <gauravsharma.mvp@gmail.com>
2017-07-21 14:14:31 +0530

    still to do

---
+++ DSA Codes/heap.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int x=1;
+	cout<<"******** MIN HEAP *************";
+	while(x!=0){
+		cout<<"1. Insert";
+		cout<<"1. Extract Min";
+		cout<<"1. Get Min";
+		cout<<"1. Insert";
+	}
+	return 0;
+}
