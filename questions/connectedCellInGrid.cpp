411d3e958ed0968dfd80488823a7d2fd7e9a96c7
Gaurav Sharma <gauravsharma.mvp@gmail.com>
2017-07-28 10:45:19 +0530

    connected cells in grid hackerrank added

--- questions/connectedCellInGrid.cpp
+++ questions/connectedCellInGrid.cpp
@@ -1,43 +1,51 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
+#include <cstring>
 using namespace std;
-bool check(int i,int j,int n,int m,int a[10][10]){
-    if(i<0 || j<0 || i>=n || j>=m)return false;
-    //if(a[i][j]==-1)return false;
-    return true;
+
+#define ROW 10
+#define COL 10
+int counter=0;
+
+bool safe(int M[][COL],int i,int j,bool visited[][COL],int n,int m){
+	
+	return ((i>=0)&&(j>=0)&&(i<n)&&(j<m)&&(M[i][j])&&(!visited[i][j]));
 }
-int dfs(int i,int j,int n,int m,int a[10][10]){
-    if(!check(i,j,n,m,a))return 0;
-    int count=1;
-    int n_i,n_j;
-    for(int x=-1;x<=1;x++){
-        for(int y=-1;y<=1;y++){
-            n_i=i+x;
-            n_j=j+y;
-            count+=dfs(n_i,n_j,n,m,a);
-        }
-    }
-    return count;
+int dfs(int M[][COL],bool visited[][COL],int i,int j,int n,int m){
+	int row[]={-1,-1,-1,0,0,1,1,1};
+	int col[]={-1,0,1,-1,1,-1,0,1};
+
+	visited[i][j]=true;
+	counter++;
+
+	for(int k=0;k<8;k++){
+		if(safe(M,i+row[k],j+col[k],visited,n,m))
+			dfs(M,visited,i+row[k],j+col[k],n,m);
+
+	}
+	return counter;
 }
-int sol(int n,int m,int a[10][10]){
-    int max=0,temp=0;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            temp=dfs(i,j,n,m,a);
-            
-            if(temp>max)max=temp;
-        }
-    }
-    return max;
+int findInslands(int M[][COL],int n,int m){
+	bool visited[ROW][COL];
+	memset(visited,0,sizeof(visited));
+
+	int max=0;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			if(M[i][j] && !visited[i][j]){
+				int temp=dfs(M,visited,i,j,n,m);
+				counter=0;
+				if(max<temp)max=temp;
+			}
+		}
+	}
+	return max;
 }
-int main() {
+int main(){
     int n,m;
     cin>>n>>m;
-    int a[10][10];
-    for(int i=0;i<n;i++)for(int j=0;j<m;j++)cin>>a[i][j];
-    cout<<sol(n,m,a);
-    return 0;
+	int M[ROW][COL];
+    for(int i=0;i<n;i++)for(int j=0;j<m;j++)cin>>M[i][j];
+    
+    cout<<findInslands(M,n,m);
+	return 0;
 }
