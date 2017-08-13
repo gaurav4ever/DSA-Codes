494455038e4302361002653c61ee911defe05cd7
Gaurav Sharma <gauravsharma.mvp@gmail.com>
2017-08-13 05:54:18 +0530

    new codes

--- Assignment/11.cpp
+++ Assignment/11.cpp
@@ -1,77 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct node{
-	struct node *next;
-	int data;
-}*start;
-
-void insert(int x){
-	struct node *n=new node;
-	n->data=x;
-	n->next=NULL;
-
-	if(start==NULL)start=n;
-	else{
-		struct node *t=start;
-		while(t->next!=NULL){
-			t=t->next;
-		}
-		t->next=n;
-	}
-}
-void printlist(){
-	struct node *n=new node;
-	n=start;
-	while(n!=NULL){
-		cout<<n->data<<" ";
-		n=n->next;
-	}
-}
-void del(int ele){
-	struct node *t=start;
-	int flag=1;
-	while(1){
-		flag=1;
-		while(t!=NULL){
-
-			if(t->next->data==ele && t->next->next!=NULL){
-				flag=0;
-				t->next->data=t->next->next->data;
-				t->next=t->next->next;
-				struct node *temp=t->next;
-				delete(temp);
-				break;
-			}else if(t->next->data==ele && t->next->next==NULL){
-				flag=0;
-				struct node *temp=t->next;
-				delete(temp);
-				t->next=NULL;
-				break;
-			}else{
-				t=t->next;
-			}
-		
-		}	
-		
-		printlist();
-		cout<<endl;
-		t=start;
-		if(flag==1)break;
-	}
-	
-}
 int main(){
 	int n,x;
 	cin>>n;
-	start=NULL;
+	list<int> ll;
+
 	while(n--){
 		cin>>x;
-		insert(x);
+		ll.push_back(x);
 	}
-	printlist();
+	list<int>::iterator it;
+	for(it=ll.begin();it!=ll.end();it++)
+		cout<<*it<<" ";
+
 	int ele;
 	cout<<"\nEnter the element to be deleted: ";cin>>ele;
-	del(ele);
-	cout<<"\nThe list after deletion: ";printlist();
+	ll.remove(ele);
+	cout<<"The list after deletion: ";
+	for(it=ll.begin();it!=ll.end();it++)
+		cout<<*it<<" ";
 	return 0;
 }
